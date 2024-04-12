3e432c616afc7bc7f229a5101a2cb87c91b6cf98
MuhammadDaniyal <daniyalshiekh166@gmail.com>
2024-04-12 21:52:22 +0500

    Create Cart UI and Landing Page UI

--- src/components/ui/StickeyBar/index.tsx
+++ src/components/ui/StickeyBar/index.tsx
@@ -5,12 +5,13 @@ import stickeySvg2 from '@/assests/svgs/stickeybar/stickeybar-2.svg';
 import stickeySvg3 from '@/assests/svgs/stickeybar/stickeybar-3.svg';
 import Link from 'next/link';
 import useLocaleRouter from '@/hooks/useLocaleRouter';
+import { PATHS } from '@/contants';
 
 const StickeyBar = () => {
 	const { url, dir, locale, changeLocale } = useLocaleRouter();
 	return (
 		<div className="fixed flex flex-col left-0 top-1/2 transform -translate-y-1/2">
-			<Link href={url('/families')} className=" bg-[#CF7475] md:p-5 p-[10px]">
+			<Link href={url(PATHS.FAMILY)} className=" bg-[#CF7475] md:p-5 p-[10px]">
 				<Image
 					src={stickeySvg1}
 					alt="stickeySvg1"
@@ -25,7 +26,7 @@ const StickeyBar = () => {
 				/>
 			</Link>
 			<Link
-				href={url('/become-sponsor')}
+				href={url(PATHS.BECOME_SPONSOR)}
 				className=" bg-[#8DAE8E] md:p-5 p-[10px]"
 			>
 				<Image
