1a99c3053c14fb854571c685e8232b2fb1ca6838
ahsan2002 <omerjee.ahsan@gmail.com>
2024-04-13 14:56:04 +0500

    fixes in donate modal

--- src/components/ui/Input/ModalInput.tsx
+++ src/components/ui/Input/ModalInput.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const ModalInput = ({label,type,placeholder}:any) => {
+const ModalInput = ({label,type,placeholder,name,value,disabled,onChange}:any) => {
+  console.log(disabled)
   return (
     <>
     <div className=' flex flex-col gap-2'>
     <h3 className=' text-sm font-semibold text-[#000000]'>{label}</h3>
-    <input className=' py-3 px-4 rounded-md bg-[#F8F8F8] w-full' type={type} placeholder={placeholder}  />
+    <input onChange={onChange} name={name} disabled={disabled} value={value} className=' py-3 px-4 rounded-md bg-[#F8F8F8] w-full focus:outline-none' type={type} placeholder={placeholder}  />
     </div>
     </>
   )
--- src/components/ui/Modals/DonateModal.tsx
+++ src/components/ui/Modals/DonateModal.tsx
@@ -6,10 +6,12 @@ import Button from '../LandingPage/Button';
 import Image from 'next/image';
 import Select from '@/components/ui/Select';
 import { countriesData } from '@/contants/countries';
+import { useFormik } from 'formik';
 // ICONS
 import { IoClose } from 'react-icons/io5';
 import Input from '@/components/ui//Input';
 import ModalInput from '../Input/ModalInput';
+import useLoggedInUser from '@/hooks/useLoggedInUser';
 
 const DonateModal: React.FC<DonateModalType> = ({
 	open,
@@ -19,6 +21,23 @@ const DonateModal: React.FC<DonateModalType> = ({
 	amount,
 	setAmount,
 }) => {
+	console.log('amou', amount);
+	const { user } = useLoggedInUser();
+	useEffect(() => {
+		DonateForm.setFieldValue('totalAmount', amount);
+	}, [amount]);
+	const DonateForm = useFormik({
+		initialValues: {
+			name: '',
+			email: '',
+			country: '',
+			totalAmount: amount ? amount : 0,
+		},
+		// validationSchema: AddFamiliesSchema,
+		onSubmit: async (values: any) => {
+			console.log('submit', values);
+		},
+	});
 	return (
 		<>
 			<Transition.Root show={open} as={Fragment}>
@@ -53,77 +72,102 @@ const DonateModal: React.FC<DonateModalType> = ({
 							>
 								<Dialog.Panel className=" flex flex-col gap-5 py-6 px-7 rounded-2xl transform overflow-hidden bg-[#fff] text-left shadow-xl transition-all md:w-[30vw] h-[90vh]">
 									{/* first div */}
-									<div className="flex justify-between items-center w-full border-b-[2px] border-[#00000080] pb-3">
-										<h2 className="  text-2xl font-semibold">Donate a Share</h2>
-										<div>
-											<IoClose
-												className=" text-3xl text-[#000] cursor-pointer"
-												onClick={() => setOpen(false)}
-											/>
+									<form onSubmit={DonateForm.handleSubmit}>
+										<div className="flex justify-between items-center w-full border-b-[2px] border-[#00000080] pb-3">
+											<h2 className="  text-2xl font-semibold">
+												Donate a Share
+											</h2>
+											<div>
+												<IoClose
+													className=" text-3xl text-[#000] cursor-pointer"
+													onClick={() => setOpen(false)}
+												/>
+											</div>
 										</div>
-									</div>
 
-									<div className=" flex flex-col gap-3 overflow-y-scroll h-[420px] scrollbarHide">
-										<ModalInput label="Name" placeholder="John" type="text" />
-										<ModalInput
-											label="Email"
-											placeholder="john@gmail.com"
-											type="email"
-										/>
-										<Select
-											title={'Country'}
-											name="country"
-											className="max-w-[800px] w-full mb-8"
-											// onChange={handleChange}
-											// value={values.country}
-											// defaultValue={t('country.default')}
-											titleColor="text-[#000000]"
-											textColor="text-[#00000080]"
-											BgColor="bg-[#F8F8F8]"
-											options={countriesData.map((country) => ({
-												label: country.name,
-												value: country.code,
-											}))}
-										/>
-										<ModalInput
-											label="Cardholder Name"
-											placeholder="JOHN WILLIAM"
-											type="text"
-										/>
-										<ModalInput
-											label="Card Number"
-											placeholder="1432 1321 1325 1235"
-											type="text"
-										/>
-										<div className=" flex gap-3">
+										<div className=" flex flex-col gap-3 overflow-y-scroll h-[420px] scrollbarHide">
 											<ModalInput
-												label="Expiry Date"
-												placeholder="07/26"
+												label="Name"
+												placeholder="John"
+												type="text"
+												name="name"
+												value={DonateForm.values?.name}
+												onChange={DonateForm.handleChange}
+											/>
+											<ModalInput
+												label="Email"
+												placeholder="john@gmail.com"
+												name="email"
+												type="email"
+												value={DonateForm.values?.email}
+												onChange={DonateForm.handleChange}
+											/>
+											<Select
+												title={'Country'}
+												name="country"
+												className="max-w-[800px] w-full mb-8"
+												onChange={DonateForm.handleChange}
+												value={DonateForm.values.country}
+												// defaultValue={t('country.default')}
+												titleColor="text-[#000000]"
+												textColor="text-[#00000080]"
+												BgColor="bg-[#F8F8F8]"
+												options={countriesData.map((country) => ({
+													label: country.name,
+													value: country.code,
+												}))}
+											/>
+											<ModalInput
+												label="Cardholder Name"
+												placeholder="JOHN WILLIAM"
 												type="text"
 											/>
+											<ModalInput
+												label="Card Number"
+												placeholder="1432 1321 1325 1235"
+												type="text"
+											/>
+											<div className=" flex gap-3">
+												<ModalInput
+													label="Expiry Date"
+													placeholder="07/26"
+													type="text"
+												/>
 
-											<ModalInput label="CVV" placeholder="551" type="number" />
-										</div>
-										<ModalInput
-											label="Enter Amount"
-											placeholder="300"
-											type="text"
-										/>
-									</div>
-
-									<div className=" flex flex-col gap-3 w-full">
-										<div className=" flex justify-between items-center">
-											<h3 className=" text-2xl font-bold">Total</h3>
-											<h3 className=" text-2xl font-bold">${amount}</h3>
-										</div>
-
-										<div className=" w-full">
-											<Button
-												title={`Confirm Payment`}
-												className=" bg-[#CF7475]"
+												<ModalInput
+													label="CVV"
+													placeholder="551"
+													type="number"
+												/>
+											</div>
+											<ModalInput
+												label="Enter Amount"
+												placeholder="300"
+												name="totalAmount"
+												type="text"
+												value={amount ? amount : DonateForm.values?.totalAmount}
+												onChange={DonateForm.handleChange}
+												disabled={user ? true : false}
 											/>
 										</div>
-									</div>
+
+										<div className=" flex flex-col gap-3 w-full">
+											<div className=" flex justify-between items-center">
+												<h3 className=" text-2xl font-bold">Total</h3>
+												<h3 className=" text-2xl font-bold">
+													${amount ? amount : DonateForm.values?.totalAmount}
+												</h3>
+											</div>
+
+											<div className=" w-full">
+												<Button
+													title={`Confirm Payment`}
+													className=" bg-[#CF7475]"
+													type="submit"
+												/>
+											</div>
+										</div>
+									</form>
 								</Dialog.Panel>
 							</Transition.Child>
 						</div>
