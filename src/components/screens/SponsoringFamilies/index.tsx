677e69d803a2d55c1823802a3fb157736f4ffad8
ahsan2002 <omerjee.ahsan@gmail.com>
2024-04-21 03:14:22 +0500

    table done

--- src/components/screens/SponsoringFamilies/index.tsx
+++ src/components/screens/SponsoringFamilies/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Table from '@/components/ui/Table';
 import { APPROVEDCOLUMN, SPONSERFAMILIESCOLUMN } from '@/contants';
 import { fetchApprovedData } from '@/hooks/useSponsorTables';
@@ -12,7 +12,10 @@ import { CustomPagination } from '@/utils/CustomPagination';
 import { SearchTable } from '@/utils/SearchTable';
 import { IoTerminal } from 'react-icons/io5';
 import { AnySrvRecord } from 'dns';
-import { FaRegListAlt } from "react-icons/fa";
+import { FaRegListAlt } from 'react-icons/fa';
+import Loader from '@/components/ui/Loader';
+import FamilyModal from '@/components/ui/Modals/FamilyModal';
+import { usePathname } from 'next/navigation';
 
 interface Row {
 	_id: number;
@@ -23,13 +26,21 @@ interface Row {
 
 const SponsoringFamilies = () => {
 	const [data, setData] = useState<any>();
+	const [isLoading, setIsLoading] = useState<boolean | string>('');
 	const initialState = {
 		pagination: { paginationModel: { pageSize: 25 } },
 		rows: data,
 	};
 	const [rows, setRows] = React.useState<any[]>([]);
+	const [currentFamilyInfo, setCurrentFamilyInfo] = useState<any>(null);
+	const [open, setOpen] = useState(false);
+	const [familiesData, setFamiliesData] = useState<any>();
+	const pathname = usePathname();
+	const currentPath = pathname?.slice(1, 3);
+	const cancelButtonRef = useRef(null);
 
 	const fetchSponserFamilies = async (sponserId: string, token: any) => {
+		setIsLoading(true);
 		const response = await getJsonWithToken(
 			`${process.env.NEXT_PUBLIC_REACT_APP_BASE_URL}/donated/${sponserId}/families`,
 			token && token,
@@ -40,14 +51,16 @@ const SponsoringFamilies = () => {
 			console.log(response.families);
 			const familiesData = response?.families
 				.filter((item: any) => {
-					return item.family !== null && item.family;
+					return item.family !== null && item.family && item.amount;
 				})
 				.map((item: any) => ({
 					...item.family,
-					id: item.family._id, // Assign _id as id
+					id: item.family._id,
+					amount: item.amount,
 				}));
 			console.log('hhj', familiesData);
 			setRows(familiesData);
+			setIsLoading(false);
 		}
 	};
 
@@ -90,7 +103,16 @@ const SponsoringFamilies = () => {
 		},
 		{
 			field: 'numberOfMartyrInFamily',
-			headerName: '# Of Martyr In Family',
+			headerName: 'No. Of Martyr In Family',
+			headerAlign: 'center',
+			align: 'center',
+			width: 200,
+			sortable: false,
+			disableColumnMenu: true,
+		},
+		{
+			field: 'amount',
+			headerName: 'Amount Donated',
 			headerAlign: 'center',
 			align: 'center',
 			width: 200,
@@ -105,9 +127,12 @@ const SponsoringFamilies = () => {
 			align: 'center',
 			disableColumnMenu: true,
 			renderCell: (params) => (
-				<div className=' flex justify-center items-center text-center'>
+				<div className=" flex justify-center items-center p-[13px]">
 					<FaRegListAlt
-						// onClick={() => handleViewUserReport(params.row as Row)}
+						onClick={() => {
+							setOpen(true);
+							handleFamiliesData(currentPath, params.row as any);
+						}}
 						className=" text-[18px] cursor-pointer"
 					/>
 				</div>
@@ -115,9 +140,39 @@ const SponsoringFamilies = () => {
 		},
 	];
 
-	useEffect(() => {
-		console.log(rows);
-	}, [rows]);
+	const handleFamiliesData = (path: string | undefined, data: any) => {
+		if (path === 'en') {
+			setFamiliesData({
+				...data,
+				breadWinnerName: data?.breadWinnerName?.inEnglish,
+				description: data?.description?.inEnglish,
+				familyMemberDetail: data?.familyMemberDetail.map((member: any) => ({
+					...member,
+					memberName: member?.memberName.inEnglish,
+				})),
+			});
+		} else if (path === 'ar') {
+			setFamiliesData({
+				...data,
+				breadWinnerName: data?.breadWinnerName?.inArabic,
+				description: data?.description?.inArabic,
+				familyMemberDetail: data?.familyMemberDetail.map((member: any) => ({
+					...member,
+					memberName: member?.memberName.inArabic,
+				})),
+			});
+		} else if (path === 'tr') {
+			setFamiliesData({
+				...data,
+				breadWinnerName: data?.breadWinnerName?.inTurkish,
+				description: data?.description?.inTurkish,
+				familyMemberDetail: data?.familyMemberDetail.map((member: any) => ({
+					...member,
+					memberName: member?.memberName.inTurkish,
+				})),
+			});
+		}
+	};
 
 	useEffect(() => {
 		const userData = getUserFromLocalStorage();
@@ -128,84 +183,81 @@ const SponsoringFamilies = () => {
 	}, []);
 
 	return (
-		<div
-			style={{
-				height: '80%',
-				width: '100%',
-				backgroundColor: '#fff',
-				border: 'none',
-				borderRadius: '10px',
-				// overflowX:'scroll'
-			}}
-		>
-			{rows ? (
-				<DataGrid
-					pagination
-					sx={{
+		<>
+			{isLoading ? (
+				<div className="flex justify-center items-center text-center">
+					<Loader />
+				</div>
+			) : (
+				<div
+					style={{
+						height: '80%',
+						width: '100%',
+						backgroundColor: '#fff',
 						border: 'none',
-						'& .MuiDataGrid-columnHeaderTitle': {
-							fontWeight: '600',
-						},
-						'& .MuiDataGrid-columnHeader': {
-							fontSize: '14px',
-						},
-						'& .MuiDataGrid-columnHeader:focus-within, .MuiDataGrid-cell:focus':
-							{
-								outline: 'none',
-							},
-						// SEARCH
-						'& .MuiBox-root': {
-							display: 'flex',
-							position: 'absolute',
-							right: 0,
-							top: '-55px',
-							background: 'white',
-							borderRadius: '12px',
-							fontSize: '14px',
-							padding: '6px 12px',
-							width: '244px',
-						},
-						'& .css-3be3ve-MuiFormControl-root-MuiTextField-root-MuiDataGrid-toolbarQuickFilter':
-							{
-								paddingBottom: '0px',
-							},
-						'& .css-68pk0f': {
-							padding: '0px',
-						},
-						'& .MuiInput-underline:before, .css-1eed5fa-MuiInputBase-root-MuiInput-root::before, .css-1eed5fa-MuiInputBase-root-MuiInput-root::after, .css-jcincl::after':
-							{
-								borderBottom: 'none !important',
-							},
-						// PAGINATION
-						'& .Mui-selected, .Mui-selected:hover': {
-							backgroundColor: 'rgba(0, 0, 0, 0.1) !important',
-							color: 'black !important',
-						},
-						'& .css-78c6dr-MuiToolbar-root-MuiTablePagination-toolbar': {
-							paddingRight: '24px',
-						},
-						'& .css-pqjvzy-MuiSvgIcon-root-MuiSelect-icon': {
-							display: 'none',
-						},
-						'& .css-194a1fa-MuiSelect-select-MuiInputBase-input.css-194a1fa-MuiSelect-select-MuiInputBase-input.css-194a1fa-MuiSelect-select-MuiInputBase-input':
-							{
-								paddingRight: '4px',
-							},
-						'& .css-194a1fa-MuiSelect-select-MuiInputBase-input ': {
-							padding: '2px 4px !important',
-							border: '1px solid',
-							borderRadius: '6px',
-							fontSize: '14px',
-						},
+						borderRadius: '10px',
+						// overflowX:'scroll'
 					}}
-					rows={rows}
-					columns={columns}
-					hideFooterPagination={false}
-					initialState={initialState}
-					slots={{ pagination: CustomPagination }}
-				/>
-			) : null}
-		</div>
+				>
+					{rows ? (
+						<DataGrid
+							pagination
+							sx={{
+								border: 'none',
+								'& .css-yrdy0g-MuiDataGrid-columnHeaderRow': {
+									background: '#742430 !important',
+								},
+								'& .MuiDataGrid-columnHeaderTitle': {
+									fontWeight: '600',
+									color: 'white !important',
+								},
+								'& .MuiDataGrid-columnHeader': {
+									fontSize: '14px',
+								},
+								'& .MuiDataGrid-columnHeader:focus-within, .MuiDataGrid-cell:focus':
+									{
+										outline: 'none',
+									},
+								// PAGINATION
+								'& .Mui-selected, .Mui-selected:hover': {
+									backgroundColor: 'rgba(0, 0, 0, 0.1) !important',
+									color: 'black !important',
+								},
+								'& .css-78c6dr-MuiToolbar-root-MuiTablePagination-toolbar': {
+									paddingRight: '24px',
+								},
+								'& .css-pqjvzy-MuiSvgIcon-root-MuiSelect-icon': {
+									display: 'none',
+								},
+								'& .css-194a1fa-MuiSelect-select-MuiInputBase-input.css-194a1fa-MuiSelect-select-MuiInputBase-input.css-194a1fa-MuiSelect-select-MuiInputBase-input':
+									{
+										paddingRight: '4px',
+									},
+								'& .css-194a1fa-MuiSelect-select-MuiInputBase-input ': {
+									padding: '2px 4px !important',
+									border: '1px solid',
+									borderRadius: '6px',
+									fontSize: '14px',
+								},
+							}}
+							rows={rows}
+							columns={columns}
+							hideFooterPagination={false}
+							initialState={initialState}
+							slots={{ pagination: CustomPagination }}
+						/>
+					) : null}
+				</div>
+			)}
+			<FamilyModal
+				setOpen={setOpen}
+				open={open}
+				isTableView={true}
+				cancelButtonRef={cancelButtonRef}
+				amount={familiesData && (familiesData?.amount as any)}
+				familyInfo={familiesData && familiesData}
+			/>
+		</>
 	);
 };
 
