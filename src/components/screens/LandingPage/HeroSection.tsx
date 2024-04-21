d69dfcbc3e96de4e75e8202ebbe08dee8ca34b84
ahsan2002 <omerjee.ahsan@gmail.com>
2024-04-21 20:05:10 +0500

    toastify customization done

--- src/components/screens/LandingPage/HeroSection.tsx
+++ src/components/screens/LandingPage/HeroSection.tsx
@@ -140,7 +140,7 @@ const HeroSection: React.FC<{ isLoggedIn?: boolean }> = ({ isLoggedIn }) => {
 								toast.error(`This feature is in progress`, {
 									toastId: 'success',
 									position: 'bottom-right',
-									autoClose: 4000,
+									autoClose: false,
 								});
 							}}
 							title={t('RegisterasFamily.title')}
