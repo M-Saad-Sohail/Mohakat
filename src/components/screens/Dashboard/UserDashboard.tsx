7ad31edd79f1b8802d30027c7861b8ede890e427
MuhammadDaniyal <daniyalshiekh166@gmail.com>
2024-04-25 01:12:37 +0500

    update currency modal

--- src/components/screens/Dashboard/UserDashboard.tsx
+++ src/components/screens/Dashboard/UserDashboard.tsx
@@ -5,11 +5,16 @@ import { getUserFromLocalStorage } from '@/utils/auth';
 import SocailSharing from './SocailSharing/SocailSharing';
 import useLocaleRouter from '@/hooks/useLocaleRouter';
 import { PATHS } from '@/contants';
+import { FaHome } from 'react-icons/fa';
 
 const UserDashboard = () => {
 	const [user, setUser] = useState<UserType | null>(null);
 	const router = useLocaleRouter();
-	
+
+	function redirectToHomePage() {
+		router.replace('/'); // Change the URL to your landing page URL
+	}
+
 	useEffect(() => {
 		const loggedInUser = getUserFromLocalStorage();
 		if (!loggedInUser) {
@@ -17,13 +22,19 @@ const UserDashboard = () => {
 			return;
 		}
 		setUser(loggedInUser);
-	  // eslint-disable-next-line react-hooks/exhaustive-deps
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	if (!user) return <></>;
 
 	return (
-		<>
+		<div className="relative">
+			<div
+				className=" absolute right-4 -top-4 text-[40px] cursor-pointer transition duration-300 ease-in-out hover:bg-gray-200 rounded-full"
+				onClick={() => redirectToHomePage()}
+			>
+				<FaHome />
+			</div>
 			<div className="text-center">
 				<h1 className="pt-2 mt-10 text-4xl font-bold leading-normal text-primary">
 					Welcome {user.name}
@@ -36,7 +47,7 @@ const UserDashboard = () => {
 				</h6>
 				<SocailSharing />
 			</div>
-		</>
+		</div>
 	);
 };
 
