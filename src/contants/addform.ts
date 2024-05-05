cb53a1bc7d15aa944ae90cff1421e04cbfd3c2a2
Zain Asif <zainasif7345@gmail.com>
2024-05-05 13:15:46 +0500

    update bug fixes

---
+++ src/contants/addform.ts
@@ -0,0 +1,209 @@
+export const genderInEnglish = [
+	{ label: 'male', value: 'male' },
+	{ label: 'female', value: 'female' },
+];
+
+export const genderInArabic = [
+	{ label: 'ذكر', value: 'ذكر' },
+	{ label: 'أنثى', value: 'أنثى' },
+];
+
+export const genderInTurkish = [
+	{ label: 'erkek', value: 'erkek' },
+	{ label: 'kadın', value: 'kadın' },
+];
+
+export const maritalStatusInEnglish = [
+	{ label: 'single', value: 'single' },
+	{ label: 'married', value: 'married' },
+	{ label: 'widow', value: 'widow' },
+	{ label: 'divorced', value: 'divorced' },
+];
+
+export const maritalStatusInArabic = [
+	{ label: 'أعزب', value: 'أعزب' },
+	{ label: 'متزوج', value: 'متزوج' },
+	{ label: 'أرملة', value: 'أرملة' },
+	{ label: 'مطلقة', value: 'مطلقة' },
+];
+
+export const maritalStatusInTurkish = [
+	{ label: 'bekar', value: 'bekar' },
+	{ label: 'evli', value: 'evli' },
+	{ label: 'dul', value: 'dul' },
+	{ label: 'boşanmış', value: 'boşanmış' },
+];
+
+export const previousResidenceInEnglish = [
+	{ label: 'Gaza', value: 'Gaza' },
+	{
+		label: 'Jabalia Camp',
+		value: 'Jabalia Camp',
+	},
+	{
+		label: 'Khan Yunis',
+		value: 'Khan Yunis',
+	},
+	{ label: 'Rafah', value: 'Rafah' },
+	{
+		label: 'Deir al-Balah',
+		value: 'Deir al-Balah',
+	},
+	{
+		label: 'Beach refugee camp',
+		value: 'Beach refugee camp',
+	},
+	{
+		label: 'Nuseirat Camp',
+		value: 'Nuseirat Camp',
+	},
+	{
+		label: 'Maghazi Camp',
+		value: 'Maghazi Camp',
+	},
+	{
+		label: 'Bureij Camp',
+		value: 'Bureij Camp',
+	},
+	{
+		label: 'Al-Shati Camp',
+		value: 'Al-Shati Camp',
+	},
+];
+
+export const previousResidenceInArabic = [
+	{ label: 'غزة', value: 'غزة' },
+	{ label: 'مخيم جباليا', value: 'مخيم جباليا' },
+	{ label: 'خان يونس', value: 'خان يونس' },
+	{ label: 'رفح', value: 'رفح' },
+	{ label: 'دير البلح', value: 'دير البلح' },
+	{ label: 'مخيم شاطئ', value: 'مخيم شاطئ' },
+	{ label: 'مخيم النصيرات', value: 'مخيم النصيرات' },
+	{ label: 'مخيم المغازي', value: 'مخيم المغازي' },
+	{ label: 'مخيم البريج', value: 'مخيم البريج' },
+	{ label: 'مخيم الشاطئ', value: 'مخيم الشاطئ' },
+];
+
+export const previousResidenceInTurkish = [
+	{ label: 'Gazze', value: 'Gazze' },
+	{ label: 'Cebeliye Kampı', value: 'Cebeliye Kampı' },
+	{ label: 'Han Yunus', value: 'Han Yunus' },
+	{ label: 'Rafah', value: 'Rafah' },
+	{ label: 'Deir al-Balah', value: 'Deir al-Balah' },
+	{ label: 'Sahil Mülteci Kampı', value: 'Sahil Mülteci Kampı' },
+	{ label: 'Nuseirat Kampı', value: 'Nuseirat Kampı' },
+	{ label: 'Maghazi Kampı', value: 'Maghazi Kampı' },
+	{ label: 'Bureij Kampı', value: 'Bureij Kampı' },
+	{ label: 'Al-Shati Kampı', value: 'Al-Shati Kampı' },
+];
+
+export const currentResidenceInEnglish = [
+	{ label: 'Gaza', value: 'Gaza' },
+	{
+		label: 'Jabalia Camp',
+		value: 'Jabalia Camp',
+	},
+	{
+		label: 'Khan Yunis',
+		value: 'Khan Yunis',
+	},
+	{ label: 'Rafah', value: 'Rafah' },
+	{
+		label: 'Deir al-Balah',
+		value: 'Deir al-Balah',
+	},
+	{
+		label: 'Beach refugee camp',
+		value: 'Beach refugee camp',
+	},
+	{
+		label: 'Nuseirat Camp',
+		value: 'Nuseirat Camp',
+	},
+	{
+		label: 'Maghazi Camp',
+		value: 'Maghazi Camp',
+	},
+	{
+		label: 'Bureij Camp',
+		value: 'Bureij Camp',
+	},
+	{
+		label: 'Al-Shati Camp',
+		value: 'Al-Shati Camp',
+	},
+];
+
+export const currentResidenceInArabic = [
+	{ label: 'غزة', value: 'غزة' },
+	{ label: 'مخيم جباليا', value: 'مخيم جباليا' },
+	{ label: 'خان يونس', value: 'خان يونس' },
+	{ label: 'رفح', value: 'رفح' },
+	{ label: 'دير البلح', value: 'دير البلح' },
+	{ label: 'مخيم شاطئ', value: 'مخيم شاطئ' },
+	{ label: 'مخيم النصيرات', value: 'مخيم النصيرات' },
+	{ label: 'مخيم المغازي', value: 'مخيم المغازي' },
+	{ label: 'مخيم البريج', value: 'مخيم البريج' },
+	{ label: 'مخيم الشاطئ', value: 'مخيم الشاطئ' },
+];
+
+export const currentResidenceInTurkish = [
+	{ label: 'Gazze', value: 'Gazze' },
+	{ label: 'Cebeliye Kampı', value: 'Cebeliye Kampı' },
+	{ label: 'Han Yunus', value: 'Han Yunus' },
+	{ label: 'Rafah', value: 'Rafah' },
+	{ label: 'Deir al-Balah', value: 'Deir al-Balah' },
+	{ label: 'Sahil Mülteci Kampı', value: 'Sahil Mülteci Kampı' },
+	{ label: 'Nuseirat Kampı', value: 'Nuseirat Kampı' },
+	{ label: 'Maghazi Kampı', value: 'Maghazi Kampı' },
+	{ label: 'Bureij Kampı', value: 'Bureij Kampı' },
+	{ label: 'Al-Shati Kampı', value: 'Al-Shati Kampı' },
+];
+
+export const currentSituationInEnglish = [
+	{ label: 'Total Destruction', value: 'Total Destruction' },
+	{ label: 'Partial Destruction', value: 'Partial Destruction' },
+	{ label: 'Intact', value: 'Intact' },
+	{ label: 'Rented', value: 'Rented' },
+];
+
+export const currentSituationInArabic = [
+	{ label: 'تدمير كامل', value: 'تدمير كامل' },
+	{ label: 'تدمير جزئي', value: 'تدمير جزئي' },
+	{ label: 'سليم', value: 'سليم' },
+	{ label: 'مستأجر', value: 'مستأجر' },
+];
+
+export const currentSituationInTurkish = [
+	{ label: 'Toplam Yıkım', value: 'Toplam Yıkım' },
+	{ label: 'Kısmi Yıkım', value: 'Kısmi Yıkım' },
+	{ label: 'Sağlam', value: 'Sağlam' },
+	{ label: 'Kiralık', value: 'Kiralık' },
+];
+
+export const lossesInWarInEnglish = [
+	{ label: 'none', value: 'none' },
+	{ label: 'car', value: 'car' },
+	{ label: 'furniture', value: 'furniture' },
+	{ label: 'store', value: 'store' },
+	{ label: 'house', value: 'house' },
+	{ label: 'business', value: 'business' },
+];
+
+export const lossesInWarInArabic = [
+	{ label: 'لا شيء', value: 'لا شيء' },
+	{ label: 'سيارة', value: 'سيارة' },
+	{ label: 'أثاث', value: 'أثاث' },
+	{ label: 'متجر', value: 'متجر' },
+	{ label: 'منزل', value: 'منزل' },
+	{ label: 'عمل تجاري', value: 'عمل تجاري' },
+];
+
+export const lossesInWarInTurkish = [
+	{ label: 'Hiçbiri', value: 'Hiçbiri' },
+	{ label: 'Araba', value: 'Araba' },
+	{ label: 'Mobilya', value: 'Mobilya' },
+	{ label: 'Mağaza', value: 'Mağaza' },
+	{ label: 'Ev', value: 'Ev' },
+	{ label: 'İş yeri', value: 'İş yeri' },
+];
